f77d567cc8a9556d5c980f147b8e0be03fb71c2c
Brenolucci <brenobertulucci@hotmail.com>
2024-06-24 17:37:27 -0300

    criando nova página de cadastro e modificando o grid de mostrar carros já puxando do backend

---
+++ .history/src/views/uikit/Media_20240624142313.vue
@@ -0,0 +1,172 @@
+<script setup>
+import { ProductService } from '@/service/ProductService';
+import { PhotoService } from '@/service/PhotoService';
+import { ref, onMounted, toRaw } from 'vue';
+import Button from 'primevue/button';
+import Skyline from '@/assets/skylineR34.jpeg'
+
+const products = ref([]);
+const images = ref([]);
+const versoes = ref([]);
+
+const carouselResponsiveOptions = ref([
+    {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    }
+]);
+
+const product = ref({
+    data: [
+		{
+			id: 1000,
+			code: 'f230fh0g3',
+			name: 'Skyline R34 - Nissan',
+			description: 'Product Description',
+			image:' @/assets/skylineR34.jpeg',
+			price: 1579000,
+			category: 'Accessories',
+			quantity: 24,
+			inventoryStatus: 'INSTOCK',
+            rating: 5,
+            orders: [
+                {
+                    id: 1000,
+                    productCode: 'f230fh0g3',
+                    date: '2020-09-13',
+                    amount: 65,
+                    quantity: 1,
+                    customer: 'David James',
+                    status: 'PENDING'
+                },
+                {
+                    id: 1001,
+                    productCode: 'f230fh0g3',
+                    date: '2020-05-14',
+                    amount: 130,
+                    quantity: 2,
+                    customer: 'Leon Rodrigues',
+                    status: 'DELIVERED'
+                },
+                {
+                    id: 1002,
+                    productCode: 'f230fh0g3',
+                    date: '2019-01-04',
+                    amount: 65,
+                    quantity: 1,
+                    customer: 'Juan Alejandro',
+                    status: 'RETURNED'
+                },
+                {
+                    id: 1003,
+                    productCode: 'f230fh0g3',
+                    date: '2020-09-13',
+                    amount: 195,
+                    quantity: 3,
+                    customer: 'Claire Morrow',
+                    status: 'CANCELLED'
+                }
+            ]
+        },
+    ]
+
+}) 
+
+const productService = new ProductService();
+const photoService = new PhotoService();
+
+const home = ref({
+    icon: 'pi pi-home'
+});
+const items = ref([
+    { label: 'Carros' }, 
+    { label: 'Usados' }, 
+    { label: 'Nissan' }, 
+]);
+
+onMounted(() => {
+    productService.getProductsSmall().then((data) => (products.value = data));
+    photoService.getImages().then((data) => (images.value = data));
+    getVersions();
+});
+
+async function getVersions(){
+    const requestOptions = {
+        method: "GET",
+        headers: {"content-type": "application/json"},
+    };
+    const response = await fetch('http://localhost/revendaCarro/hmtl/src/controllers/CriarVersao.php');
+    const data = await response.json();
+
+    versoes.value = data;
+
+    console.log(toRaw(versoes.value));
+
+}
+
+const getSeverity = (status) => {
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+
+        case 'LOWSTOCK':
+            return 'warning';
+
+        case 'OUTOFSTOCK':
+            return 'danger';
+
+        default:
+            return null;
+    }
+};
+</script>
+
+<template>
+    <Breadcrumb class="mb-5" :home="home" :model="items" />
+    <div class="mb-5 ml-5">
+        <h1 class="text-xl font-semibold font" >Carros usados, seminovos e novos em Florianópolis/SC</h1>
+        <span>17.321 resultados encontrados</span>
+    </div>
+    <Carousel class="mx-5" :value="products" :numVisible="5" :numScroll="5" :responsiveOptions="carouselResponsiveOptions">
+        <template #item="slotProps">
+            <div v-for="versao in vesoes">
+                <div class="border-1 surface-border border-round w-auto m-2 ">
+                    <div class="mb-3">
+                        <div class="relative mx-auto">
+                            <img :src="Skyline" :alt="versoes[0].nome" class="w-full" />
+                            <Tag :value="product.data[0].inventoryStatus" :severity="true" class="absolute" style="left: 3px; top: 5px" />
+                        </div>
+                    </div>
+                    <div class="p-3">
+    
+                        <div class="mb-2 font-semibold">{{ versoes[0].modelo.marca.nome.toUpperCase() }} {{ versoes[0].modelo.nome.toUpperCase() }}</div>
+                        <span class="mb-30">{{versoes[0].nome}}</span>
+                        <div class="justify-content-between align-items-center mb-2">
+                            <div class="mt-3 font-semibold text-xl">R$ {{ versoes[0].preco }}</div>
+                            <div class="flex justify-content-between align-items-center mb-2">
+                                <span>{{ versoes[0].ano }}/{{ versoes[0].ano_modelo }}</span>
+                                <span>{{ versoes[0].quilometragem }}KM</span>
+                            </div>
+                            <span class="flex justify-center">
+                                <Button class="mx-6 mt-4 mb-2">Ver detalhes</Button>
+                            </span>
+                            <hr/>
+                            <span class="flex justify-center">{{ versoes[0].localizacao }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Carousel>
+</template>
